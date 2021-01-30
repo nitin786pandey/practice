81485d4e25872f1bc159aea0ad354bfc2b68db82
nitin786pandey <nitin786pandey3@gmail.com>
2021-01-30 22:53:20 +0530

    nitin

---
+++ practice 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int sum;
+	
+	cout<<"Hello World \n";
+	cin>>sum;
+	int i;
+	i=0;
+	for (i=0;1<10;i++)
+	{
+		sum=sum+i;
+	}
+    cout<<"Hello World \n" <<sum;
+   	
+}
